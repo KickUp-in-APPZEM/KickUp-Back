c8e019c25ab12e4c5fb3cf307c43129d79925059
leegunwoooo <leegunwoo0325@gmail.com>
2024-12-21 15:40:04 +0900

    first commit

---
+++ src/test/java/appjem/alarm/AlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package appjem.alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
